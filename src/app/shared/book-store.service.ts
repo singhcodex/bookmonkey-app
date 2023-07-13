43d8786b6d8f8a1793ec10507dc59e6b572acada
singhcodex <coder69developer@gmail.com>
2023-07-13 11:41:16 +0200

    Error handling applied on service

--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Book } from './book';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -13,7 +13,12 @@ export class BookStoreService {
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<Book[]> {
-    return this.http.get<Book[]>(`${this.apiUrl}/books`);
+    return this.http.get<Book[]>(`${this.apiUrl}/books`).pipe(
+      catchError(err => {
+        console.error(err);
+        return of([]);
+      })
+    );
   }
 
   getSingle(isbn: string): Observable<Book> {
@@ -25,6 +30,11 @@ export class BookStoreService {
   }
 
   getAllSearch(term: string): Observable<Book[]>{
-    return this.http.get<Book[]>(`${this.apiUrl}/books/search/${term}`);
+    return this.http.get<Book[]>(`${this.apiUrl}/books/search/${term}`).pipe(
+      catchError(err => {
+        console.error(err);
+        return of([]);
+      })
+    );
   }
 }
