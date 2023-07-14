ff827d55d8edcd50673195a63e07b155feb8eb73
singhcodex <coder69developer@gmail.com>
2023-07-14 14:54:22 +0200

    change book module and routes

--- src/app/books/books.routes.ts
+++ src/app/books/books.routes.ts
@@ -1,5 +1,4 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {  Routes } from '@angular/router';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 
