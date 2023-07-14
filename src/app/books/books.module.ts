7e5cf8e202a8b9c836ee7116f617a59cb9c92888
singhcodex <coder69developer@gmail.com>
2023-07-14 13:06:24 +0200

    custom pipe and date pipe applied

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -5,12 +5,16 @@ import { BooksRoutingModule } from './books-routing.module';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { BookListItemComponent } from './book-list-item/book-list-item.component';
+import { IsbnPipe } from './shared/isbn.pipe';
+import { ConfirmDirective } from './shared/confirm.directive';
 
 @NgModule({
   declarations: [
     BookListComponent,
     BookDetailsComponent,
     BookListItemComponent,
+    IsbnPipe,
+    ConfirmDirective,
   ],
   imports: [CommonModule, BooksRoutingModule],
 })
---
+++ src/app/books/shared/confirm.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[bmConfirm]'
+})
+export class ConfirmDirective {
+
+  constructor() { }
+
+}
